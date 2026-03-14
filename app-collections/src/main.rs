130c9463da07c6d42dc5a9ffdd53ffa03e4f6dd6
diannaaa <diannaaav@gmail.com>
2026-03-14 15:42:00 +0800

    complete app-collections exercise

--- app-collections/src/main.rs
+++ app-collections/src/main.rs
@@ -1,23 +1,35 @@
 #![cfg_attr(feature = "axstd", no_std)]
 #![cfg_attr(feature = "axstd", no_main)]
 
+#[macro_use]
 #[cfg(feature = "axstd")]
-extern crate alloc;
+extern crate axstd as std;
 
 #[cfg(feature = "axstd")]
-use alloc::string::String;
-#[cfg(feature = "axstd")]
-use alloc::vec;
-
-#[cfg(feature = "axstd")]
-use axstd::println;
+use std::collections::HashMap;
 
 #[cfg_attr(feature = "axstd", unsafe(no_mangle))]
 fn main() {
-    let s = String::from("Hello, axalloc!");
-    println!("Alloc String: \"{}\"", s);
-
-    let mut v = vec![0, 1, 2];
-    v.push(3);
-    println!("Alloc Vec: {:?}", v);
+    println!("Running memory tests...");
+    test_hashmap();
+    println!("Memory tests run OK!");
 }
+
+#[cfg(feature = "axstd")]
+fn test_hashmap() {
+    const N: u32 = 50_000;
+    let mut m = HashMap::new();
+    for value in 0..N {
+        let key = format!("key_{value}");
+        m.insert(key, value);
+    }
+    for (k, v) in m.iter() {
+        if let Some(k) = k.strip_prefix("key_") {
+            assert_eq!(k.parse::<u32>().unwrap(), *v);
+        }
+    }
+    println!("test_hashmap() OK!");
+}
+
+#[cfg(not(feature = "axstd"))]
+fn test_hashmap() {}
